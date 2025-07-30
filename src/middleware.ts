205231834a0bf997972bce155f80d5ac58d87b2c
Sindiswa Kumalo <masitlaem@gmail.com>
2025-07-30 03:49:36 +0200

    fix: use auth.protect() for Clerk v5

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/']);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) auth().protect();
+  if (!isPublicRoute(req)) auth.protect(); // <- no () after auth
 });
 
 export const config = {
